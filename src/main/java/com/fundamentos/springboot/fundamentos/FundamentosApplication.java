dbff79e2382eed36f96b4f30ebc1ca9b64f44118
jbeltran402 <juandavid72009@gmail.com>
2022-01-30 20:51:10 -0600

    Se implemento logs de tipo Error, Info o Debug

--- src/main/java/com/fundamentos/springboot/fundamentos/FundamentosApplication.java
+++ src/main/java/com/fundamentos/springboot/fundamentos/FundamentosApplication.java
@@ -4,6 +4,9 @@ import com.fundamentos.springboot.fundamentos.bean.MyBean;
 import com.fundamentos.springboot.fundamentos.bean.MyBeanWithDependency;
 import com.fundamentos.springboot.fundamentos.bean.MyBeanWithProperties;
 import com.fundamentos.springboot.fundamentos.component.ComponentDependency;
+import org.apache.juli.logging.Log;
+import org.apache.juli.logging.LogFactory;
+import org.slf4j.ILoggerFactory;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -12,27 +15,36 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class FundamentosApplication implements CommandLineRunner {
 
-	private ComponentDependency componentDependency;
-	private MyBean myBean;
-	private MyBeanWithDependency myBeanWithDependency;
-	private MyBeanWithProperties myBeanWithProperties;
+    private final Log LOGGER = LogFactory.getLog(FundamentosApplication.class);
 
-	public FundamentosApplication(@Qualifier("componentTowImplement") ComponentDependency componentDependency, MyBean myBean, MyBeanWithDependency myBeanWithDependency, MyBeanWithProperties myBeanWithProperties){
-		this.componentDependency = componentDependency;
-		this.myBean = myBean;
-		this.myBeanWithDependency = myBeanWithDependency;
-		this.myBeanWithProperties = myBeanWithProperties;
-	}
+    private ComponentDependency componentDependency;
+    private MyBean myBean;
+    private MyBeanWithDependency myBeanWithDependency;
+    private MyBeanWithProperties myBeanWithProperties;
 
-	public static void main(String[] args) {
-		SpringApplication.run(FundamentosApplication.class, args);
-	}
+    public FundamentosApplication(@Qualifier("componentTowImplement") ComponentDependency componentDependency, MyBean myBean, MyBeanWithDependency myBeanWithDependency, MyBeanWithProperties myBeanWithProperties) {
+        this.componentDependency = componentDependency;
+        this.myBean = myBean;
+        this.myBeanWithDependency = myBeanWithDependency;
+        this.myBeanWithProperties = myBeanWithProperties;
+    }
 
-	@Override
-	public void run(String... args) throws Exception {
-		componentDependency.HolaMundo();
-		myBean.print();
-		myBeanWithDependency.printWhithDependency();
-		System.out.println(myBeanWithProperties.function());
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(FundamentosApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        componentDependency.HolaMundo();
+        myBean.print();
+        myBeanWithDependency.printWhithDependency();
+        System.out.println(myBeanWithProperties.function());
+        try {
+            int VariableError = 10 / 0;
+            LOGGER.debug("El resultado es :" + VariableError);
+        } catch (Exception e) {
+            LOGGER.error("Esto es un error en la suma, se esta dividiendo por cero " + e.getStackTrace());
+        }
+        /* TODO -> generar mis propios Logs en los archivos que tengo*/
+    }
 }
--- src/main/java/com/fundamentos/springboot/fundamentos/bean/MyBeanWithDependencyImplement.java
+++ src/main/java/com/fundamentos/springboot/fundamentos/bean/MyBeanWithDependencyImplement.java
@@ -1,6 +1,11 @@
 package com.fundamentos.springboot.fundamentos.bean;
 
+import org.apache.juli.logging.Log;
+import org.apache.juli.logging.LogFactory;
+
 public class MyBeanWithDependencyImplement implements MyBeanWithDependency{
+
+    Log LOGGER = LogFactory.getLog(MyBeanWithDependencyImplement.class);
 
     private MyOperation myOperation;
 
@@ -10,7 +15,9 @@ public class MyBeanWithDependencyImplement implements MyBeanWithDependency{
 
     @Override
     public void printWhithDependency() {
+        LOGGER.info("Se Esta Generando la operacion en el metodo printWhithDependency");
         int number = 10;
+        LOGGER.debug("El prametro enviado en la dependencia operacion es :"+number);
         System.out.println("Hola desde la implementacion de un bean con dependencia \n" +
                 "Este el es resultado de la suma : "+ myOperation.sum(number));
     }
